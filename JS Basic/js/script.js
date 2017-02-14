8620726e99b46c4a5e7625e5b252d43e5d8941c3
Lars Wagemans <Lars Wagemans>
2017-02-14 16:03:06 +0100

    added workinfo

--- JS Basic/js/script.js
+++ JS Basic/js/script.js
@@ -1,6 +1,19 @@
-//variables
+//get name
+var name = prompt('What is your name?'); 
+while(name===''){
+	alert('Please fill in your name');
+	var name = prompt('What is your name'); 
+}
 
+//welcome
+document.write('<h2>Welcome ' + name + '!</h2>');	
 
+//week
+arrayWeekday = ['Monday', 'Tuesday'];
 
-//prompt
-var name = prompt("What is your name"); 
+//result
+for(i=0; i<2; i++){
+document.write('<p>' + name + ' has to work on ' + arrayWeekday[i] + '</p>');
+}
+document.write('This week ' + name + ' has to work ' + i + ' days this week.');
+
