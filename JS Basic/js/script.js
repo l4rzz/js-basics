25084ba58ee047fb7c8b112c76e3bfab37a4c586
larswagemans <amensites@gmail.com>
2017-02-15 14:22:06 +0100

    Final Functionalities

--- JS Basic/js/script.js
+++ JS Basic/js/script.js
@@ -1,19 +1,66 @@
-//get name
-var name = prompt('What is your name?'); 
-while(name===''){
-	alert('Please fill in your name');
-	var name = prompt('What is your name'); 
+/*
+*
+* Basics of JS with the presentation of my intership company
+*
+*/
+
+//get names
+var firstName = prompt('What is your firstname?'); 
+while(firstName === ''){
+	alert('This field is required');
+	firstName = prompt('What is your firstname?'); 
 }
+
+var lastName = prompt('What is your lastname?');
+while(lastName === ''){
+	alert('This field is required');
+	lastName = prompt('What is your lastname'); 		
+}
+
+//add space
+var space = ' ';
+
+//concat names
+var fullName = firstName.concat(space,lastName);
+
+function capitalizeFirstLetter(string){
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+capitalizeFirstLetter(firstName);
+capitalizeFirstLetter(lastName);
 
 //welcome
-document.write('<h2>Welcome ' + name + '!</h2>');	
+	document.write('<h2>Welcome ' + fullName + '!</h2>');	
 
-//week
-arrayWeekday = ['Monday', 'Tuesday'];
+//manage workweek
+var employeeObject = {
+	workdayArray:[
+	'Monday',
+	'Wednesday',
+	'Friday'
+	]
+};
 
 //result
-for(i=0; i<2; i++){
-document.write('<p>' + name + ' has to work on ' + arrayWeekday[i] + '</p>');
+for(i = 0; i < 3; i++){
+document.write('<p><strong>' + capitalizeFirstLetter(fullName) + '</strong> has to work this week on <strong>' + employeeObject.workdayArray[i] + '</strong>.</p>');
 }
-document.write('This week ' + name + ' has to work ' + i + ' days this week.');
 
+document.write('<p>' + capitalizeFirstLetter(fullName) + ' has to work <strong>'+ employeeObject.workdayArray.length + ' days </strong> this week.</p>');
+
+//math
+
+//calculates % of workdays per week
+var totalWorkweek = 5;
+var averageDays = employeeObject.workdayArray.length / totalWorkweek * 100;
+
+//validate amount
+if(averageDays > 100){
+	document.write(capitalizeFirstLetter(fullName) + ' can\'t work more days than ' + totalWorkweek + 'days per week')
+}
+
+else
+{
+	document.write('This workweek <strong>' + capitalizeFirstLetter(fullName) + '</strong> works ' + averageDays + '% of this workweek.');
+}
